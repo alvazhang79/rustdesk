72db37a02bccf1086311eb86ba1ebb92bb9d1236
alvazhang79 <45260445+alvazhang79@users.noreply.github.com>
2024-08-27 15:48:07 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk-server1.namiyun.cn"];
-pub const PUBLIC_RS_PUB_KEY: &str = "kNn2I2cJ+pNdPYXLK1iMN0cOgKimPu3eKDGlJgl0xGk=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &[""];
+pub const PUBLIC_RS_PUB_KEY: &str = "";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
